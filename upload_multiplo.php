b616365a98cf478d5baf32b16bd0b58f4578dd24
Jeandra Cristina <jeandra8789@gmail.com>
2021-11-09 17:41:32 -0300

    upload múltiplo de arquivos

---
+++ upload_multiplo.php
@@ -0,0 +1,60 @@
+<?php
+
+if (isset($_POST['bt_enviar'])) {
+    $tipos_permitidos = [
+        'jpg', 'jpeg', 'gif', 'png',
+        'JPG', 'JPEG', 'GIF', 'PNG'
+    ];
+
+
+    $quantidade_arquivo = count($_FILES['arquivo']['name']);
+    $contador = 0;
+
+    while ($contador < $quantidade_arquivo) {
+        $extensao = pathinfo(
+            $_FILES['arquivo']['name'][$contador],
+            PATHINFO_EXTENSION
+        );
+
+        if (in_array($extensao, $tipos_permitidos)) {
+            $pasta = "arquivos/";
+            $temporario = $_FILES['arquivo']['tmp_name']
+            [$contador];
+            $novo_nome = uniqid().".$extensao";
+
+            #fazer upload do arquivo.
+            if (move_uploaded_file($temporario, $pasta . $novo_nome)) {
+                echo "<p>Upload realizado com sucesso!</p>";
+            } else {
+                echo "<p>Falha no carregamento!</p>";
+            } # fim da realização do upload.
+        } else {
+            echo "<p>Tipo de arquivo não permitido.</p>";
+        } #fim if array
+        $contador++; #incremento do contador while
+    } #fim while
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upload de Arquivo.</title>
+</head>
+
+<body>
+
+    <form action="upload_multiplo.php" method="POST" enctype="multipart/form-data">
+        <input type="file" name="arquivo[]" multiple />
+        <button type="submit" name="bt_enviar">Enviar </button>
+
+    </form>
+
+</body>
+
+</html>
